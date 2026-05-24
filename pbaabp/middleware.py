406a878b1b355cf3951dcc87b48c47737011708e
Ee Durbin <ewdurbin@gmail.com>
2026-05-24 14:27:16 -0400

    set the user cookie for event sign-ins on login

--- pbaabp/middleware.py
+++ pbaabp/middleware.py
@@ -1,11 +1,25 @@
 import zoneinfo
 
 from django.conf import settings
+from django.contrib.auth.signals import user_logged_in
+from django.dispatch import receiver
 from django.urls import NoReverseMatch, reverse
 from django.utils import timezone
 
-from events.signin_cookie import delete_event_signin_cookie
+from events.signin_cookie import (
+    EVENT_SIGNIN_COOKIE_TYPE_USER,
+    delete_event_signin_cookie,
+    set_event_signin_cookie,
+)
 from pbaabp.forms import NewsletterSignupForm
+
+
+EVENT_SIGNIN_LOGIN_USER_ATTR = "_event_signin_login_user"
+
+
+@receiver(user_logged_in, dispatch_uid="set_event_signin_cookie_on_login")
+def set_event_signin_cookie_on_login(sender, request, user, **kwargs):
+    setattr(request, EVENT_SIGNIN_LOGIN_USER_ATTR, user)
 
 
 class ClearEventSignInCookieOnLogoutMiddleware:
@@ -14,6 +28,13 @@ class ClearEventSignInCookieOnLogoutMiddleware:
 
     def __call__(self, request):
         response = self.get_response(request)
+        login_user = getattr(request, EVENT_SIGNIN_LOGIN_USER_ATTR, None)
+        if login_user is not None:
+            set_event_signin_cookie(
+                response,
+                {"type": EVENT_SIGNIN_COOKIE_TYPE_USER, "id": login_user.id},
+            )
+
         try:
             logout_path = reverse("account_logout")
         except NoReverseMatch:
